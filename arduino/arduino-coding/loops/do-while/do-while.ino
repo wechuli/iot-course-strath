44d8dc0010300eeccf3cc86dff977823d56e2902
Wechuli <wechulipaul@yahoo.com>
2018-02-22 15:40:27 +0300

    changes

--- arduino/arduino-coding/loops/do-while/do-while.ino
+++ arduino/arduino-coding/loops/do-while/do-while.ino
@@ -2,10 +2,16 @@ void setup(){
     Serial.begin(9600);
 }
 void loop(){
-    float number 45.6;
+    int number=10;
     do {
         Serial.println("The number is still big");
-        number=number-10;
-    }
-    while(number<50);///Remeber our infinite loop? This solves the problem as the statement will become false at one point
+        number=number+1;
+    }while(number<20);///Remeber our infinite loop? This solves the problem as the statement will 
+    //become false at one point
+
+
+//we add here a very long delay for us to see what's happening, otherwise the loop function will keep 
+//repeating itself forever and ever -:)
+
+    delay(5000);
 }
